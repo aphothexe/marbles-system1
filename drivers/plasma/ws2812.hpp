19879ceb191360200fbdb041afe65195f79a8536
aphothexe <109735319+aphothexe@users.noreply.github.com>
2021-01-18 16:43:08 +0000

    [maven-release-plugin] prepare release 0.7.0

--- drivers/plasma/ws2812.hpp
+++ drivers/plasma/ws2812.hpp
@@ -1,3 +1,4 @@
+
 /**
  * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
  *
@@ -6,7 +7,7 @@
 
 /*
 This code is significantly modified from the PIO apa102 example
-found here: https://github.com/raspberrypi/pico-examples/tree/master/pio/apa102
+found here: https://github.com/raspberrypi/pico-examples/tree/master/pio/ws2812
 */
 
 #pragma once
@@ -14,7 +15,7 @@ found here: https://github.com/raspberrypi/pico-examples/tree/master/pio/apa102
 #include <math.h>
 #include <cstdint>
 
-#include "apa102.pio.h"
+#include "ws2812.pio.h"
 
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
@@ -25,43 +26,52 @@ found here: https://github.com/raspberrypi/pico-examples/tree/master/pio/apa102
 
 namespace plasma {
 
-    class APA102 {
+    class WS2812 {
         public:
-            static const uint DEFAULT_SERIAL_FREQ = 20 * 1000 * 1000; // 20MHz
+            static const uint SERIAL_FREQ_400KHZ = 400000;
+            static const uint SERIAL_FREQ_800KHZ = 800000;
+            static const uint DEFAULT_SERIAL_FREQ = SERIAL_FREQ_800KHZ;
+            enum class COLOR_ORDER {
+                RGB,
+                RBG,
+                GRB,
+                GBR,
+                BRG,
+                BGR
+            };
 #pragma pack(push, 1)
             union alignas(4) RGB {
                 struct {
-                    uint8_t sof = 0b11101111;
-                    uint8_t b;
-                    uint8_t g;
                     uint8_t r;
+                    uint8_t g;
+                    uint8_t b;
+                    uint8_t w = 0b00000000;
                 } ;
                 uint32_t srgb;
                 void operator=(uint32_t v) {
                     srgb = v;
                 };
-                void brightness(uint8_t b) {
-                    sof = 0b11100000 | b;
-                };
-                void rgb(uint8_t r, uint8_t g, uint8_t b) {
+                void rgb(uint8_t r, uint8_t g, uint8_t b, uint8_t w=0) {
                     this->r = r;
                     this->g = g;
-                    this->b = b;;
-                }
-                RGB() : sof(0b11101111), b(0), g(0), r(0) {};
+                    this->b = b;
+                    this->w = w;
+                };
+                RGB() : r(0), g(0), b(0), w(0) {};
             };
 #pragma pack(pop)
             RGB *buffer;
             uint32_t num_leds;
+            COLOR_ORDER color_order;
 
-            APA102(uint num_leds, PIO pio, uint sm, uint pin_dat, uint pin_clk, uint freq=DEFAULT_SERIAL_FREQ, RGB* buffer=nullptr);
-            ~APA102() {
+            WS2812(uint num_leds, PIO pio, uint sm, uint pin, uint freq=DEFAULT_SERIAL_FREQ, bool rgbw=false, COLOR_ORDER color_order=COLOR_ORDER::GRB, RGB* buffer=nullptr);
+            ~WS2812() {
                 stop();
                 clear();
                 update(true);
                 dma_channel_unclaim(dma_channel);
                 pio_sm_set_enabled(pio, sm, false);
-                pio_remove_program(pio, &apa102_program, pio_program_offset);
+                pio_remove_program(pio, &ws2812_program, pio_program_offset);
 #ifndef MICROPY_BUILD_TYPE
                 // pio_sm_unclaim seems to hardfault in MicroPython
                 pio_sm_unclaim(pio, sm);
@@ -75,8 +85,8 @@ namespace plasma {
             bool stop();
             void update(bool blocking=false);
             void clear();
-            void set_hsv(uint32_t index, float h, float s, float v);
-            void set_rgb(uint32_t index, uint8_t r, uint8_t g, uint8_t b, bool gamma=true);
+            void set_hsv(uint32_t index, float h, float s, float v, uint8_t w=0);
+            void set_rgb(uint32_t index, uint8_t r, uint8_t g, uint8_t b, uint8_t w=0, bool gamma=true);
             void set_brightness(uint8_t b);
             RGB get(uint32_t index) {return buffer[index];};
 
