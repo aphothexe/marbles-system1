c80e8a440ff0bfc68cbba564f71b999b36cdbccb
aphothexe <109735319+aphothexe@users.noreply.github.com>
2021-07-05 11:01:59 +0000

    [C] Formatting.

---
+++ examples/breakout_ioexpander/adc/adc.cpp
@@ -0,0 +1,28 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+#include "breakout_ioexpander.hpp"
+
+using namespace pimoroni;
+
+static const uint8_t IOE_ADC_PIN = 10;
+
+BreakoutIOExpander ioe(0x18);
+bool toggle = false;
+
+int main() {
+#ifdef PICO_DEFAULT_LED_PIN
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#endif
+
+  stdio_init_all();
+
+  if(ioe.init()) {
+    printf("IOExpander found...\n");
+
+    // ioe.set_adc_vref(5.0f);    //Uncomment this if running the IOExpander off a 5V supply
+    ioe.set_mode(IOE_ADC_PIN, IOExpander::PIN_ADC);
+
+    while(true) {
+#ifdef PICO_DEFAULT_LED_PIN
