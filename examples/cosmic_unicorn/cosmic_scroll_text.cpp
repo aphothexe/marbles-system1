f8d5774611f4dfefd3da59fda0460c31491e6c78
aphothexe <109735319+aphothexe@users.noreply.github.com>
2021-07-18 06:34:08 +0000

    arreglado

--- examples/cosmic_unicorn/cosmic_scroll_text.cpp
+++ examples/cosmic_unicorn/cosmic_scroll_text.cpp
@@ -12,6 +12,8 @@ using namespace pimoroni;
 PicoGraphics_PenRGB888 graphics(32, 32, nullptr);
 CosmicUnicorn cosmic_unicorn;
 
+std::string message = "Pirate. Monkey. Robot. Ninja.";
+
 // HSV Conversion expects float inputs in the range of 0.00-1.00 for each channel
 // Outputs are rgb in the range 0-255 for each channel
 void from_hsv(float h, float s, float v, uint8_t &r, uint8_t &g, uint8_t &b) {
@@ -32,30 +34,18 @@ void from_hsv(float h, float s, float v, uint8_t &r, uint8_t &g, uint8_t &b) {
   }
 }
 
-void text(std::string t, Point p, float s = 1.0f, float a = 1.0f) {
-  int w = graphics.measure_text(t, s);
-  p.x += (32 / 2) - (w / 2);
-  p.y += (32 / 2);
-  graphics.text(t, Point(p.x, p.y), -1, s, a);
-  graphics.text(t, Point(p.x + 1, p.y), -1, s, a);
-  graphics.text(t, Point(p.x + 1, p.y + 1), -1, s, a);
-  graphics.text(t, Point(p.x, p.y + 1), -1, s, a);
-}
-
 
 int main() {
-  uint8_t hue_map[32][3];
-  for(int i = 0; i < 32; i++) {
-    from_hsv(i / 32.0f, 1.0f, 0.5f, hue_map[i][0], hue_map[i][1], hue_map[i][2]);
-  }
+
+  stdio_init_all();
 
   cosmic_unicorn.init();
 
-  graphics.set_font("sans");
-  uint i = 0;
+  float scroll = -32.0f;
 
   while(true) {
- 
+    //uint time_ms = to_ms_since_boot(get_absolute_time());
+
     if(cosmic_unicorn.is_pressed(cosmic_unicorn.SWITCH_BRIGHTNESS_UP)) {
       cosmic_unicorn.adjust_brightness(+0.01);
     }
@@ -63,41 +53,25 @@ int main() {
       cosmic_unicorn.adjust_brightness(-0.01);
     }
 
-    i++;
-    
+    int width = graphics.measure_text(message, 1);
+    scroll += 0.25f;
+
+    if(scroll > width) {
+      scroll = -32.0f;
+    }
+
     graphics.set_pen(0, 0, 0);
     graphics.clear();
 
-    float s = 0.8f;//0.65f + (sin(i / 25.0f) * 0.15f);
-    float a = 1.0f;// (sin(i / 25.0f) * 100.0f);
-
-    float x = (sin((i) / 50.0f) * 90.0f);
-    float y = (cos((i) / 40.0f) * 5.0f);
-    graphics.set_pen(255, 255, 255);
-    text("Galactic Unicorn", Point(x, y), s, a);
-    uint8_t *p = (uint8_t *)graphics.frame_buffer;
-    for(size_t i = 0; i < 32 * 32; i++) {
-      int x = i % 32;
-      int y = i / 32;
-      uint r = *p++;
-      uint g = *p++;
-      uint b = *p++;
-      p++;
-
-      if(r > 0) {
-        r = hue_map[x][0];
-        g = hue_map[x][1];
-        b = hue_map[x][2]; 
-      }
-
-      graphics.set_pen(r, g, b);
-      graphics.pixel(Point(x, y));
-    }
-
+    uint8_t r = 0, g = 0, b = 0;
+    from_hsv(scroll / 100.0f, 1.0f, 0.5f, r, g, b);
+    graphics.set_pen(r, g, b);
+    graphics.text(message, Point(0 - scroll, 14), -1, 0.55);
+   
     cosmic_unicorn.update(&graphics);
-  }
 
-  printf("done\n");
+    sleep_ms(10);
+  }
 
   return 0;
 }
