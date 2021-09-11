dc64c0bfdb7adb5414eda0367017576612827098
aphothexe <109735319+aphothexe@users.noreply.github.com>
2021-09-11 21:07:45 +0000

    Updating the readme.

--- examples/pico_enc_explorer/pico_enc_explorer.cpp
+++ examples/pico_enc_explorer/pico_enc_explorer.cpp
@@ -61,4 +61,58 @@ void count_changed(int16_t count) {
   }
 
   {
-    // Shouldn't reall
+    // Shouldn't really use create_pen in-line.
+    // In default (RGB332) palette mode this will lookup the nearest 8-bit colour
+    graphics.set_pen(graphics.create_pen(p.r, p.g, p.b));
+    std::ostringstream ss;
+    ss << "#";
+    ss << std::uppercase << std::hex << std::setfill('0') << std::setw(2) << (int)(p.r);
+    ss << std::uppercase << std::hex << std::setfill('0') << std::setw(2) << (int)(p.g);
+    ss << std::uppercase << std::hex << std::setfill('0') << std::setw(2) << (int)(p.b);
+    std::string s(ss.str());
+    graphics.text(s, Point(10, 190), 220, 5);
+  }
+  st7789.update(&graphics);
+}
+
+int main() {
+#ifdef PICO_DEFAULT_LED_PIN
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#endif
+
+  stdio_init_all();
+
+  int16_t count = 0;
+  if(enc.init()) {
+    printf("Encoder found...\n");
+
+    count_changed(count);
+    enc.clear_interrupt_flag();
+
+    while(true) {
+#ifdef PICO_DEFAULT_LED_PIN
+      gpio_put(PICO_DEFAULT_LED_PIN, toggle);
+#endif
+      toggle = !toggle;
+
+      if(enc.get_interrupt_flag()) {
+        count = enc.read();
+        enc.clear_interrupt_flag();
+
+        while(count < 0)
+          count += STEPS_PER_REV;
+
+        count_changed(count);
+      }
+    }
+  }
+  else {
+    printf("Encoder not found :'(\n");
+#ifdef PICO_DEFAULT_LED_PIN
+    gpio_put(PICO_DEFAULT_LED_PIN, true);
+#endif
+  }
+
+  return 0;
+}
