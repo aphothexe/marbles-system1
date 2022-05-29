416b532df1bef9d033d70a7287a599b064f3fc18
aphothexe <109735319+aphothexe@users.noreply.github.com>
2022-05-29 10:48:35 +0000

    #current work

--- examples/pico_wireless/demo.cmake
+++ examples/pico_wireless/demo.cmake
@@ -10,3 +10,6 @@ pico_enable_stdio_uart(wireless_time 1)
 
 # Pull in pico libraries that we need
 target_link_libraries(wireless_time pico_stdlib pico_wireless)
+
+# create map/bin/hex file etc.
+pico_add_extra_outputs(wireless_time)
