0396bec8ac84c56ea5f2cb840278ddaa38d1e7ef
aphothexe <109735319+aphothexe@users.noreply.github.com>
2022-09-28 09:34:30 +0000

    import changes

---
+++ micropython/examples/cosmic_unicorn/launch/rainbow.py
@@ -0,0 +1,26 @@
+import math
+from cosmic import CosmicUnicorn
+
+graphics = None
+palette = None
+
+width = CosmicUnicorn.WIDTH
+height = CosmicUnicorn.HEIGHT
+
+
+@micropython.native  # noqa: F821
+def from_hsv(h, s, v):
+    i = math.floor(h * 6.0)
+    f = h * 6.0 - i
+    v *= 255.0
+    p = v * (1.0 - s)
+    q = v * (1.0 - f * s)
+    t = v * (1.0 - (1.0 - f) * s)
+
+    i = int(i) % 6
+    if i == 0:
+        return int(v), int(t), int(p)
+    if i == 1:
+        return int(q), int(v), int(p)
+    if i == 2:
+        return int
