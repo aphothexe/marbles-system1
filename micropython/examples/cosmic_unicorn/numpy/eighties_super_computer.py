ff930422d35db449a78fc270971f758f9c621ad7
aphothexe <109735319+aphothexe@users.noreply.github.com>
2022-09-29 13:04:22 +0000

    floppy: do put_disk on current dr if blk_init_queue fails

--- micropython/examples/cosmic_unicorn/numpy/eighties_super_computer.py
+++ micropython/examples/cosmic_unicorn/numpy/eighties_super_computer.py
@@ -49,3 +49,67 @@ def update():
     for _ in range(NUMBER_OF_LIGHTS):
         x = random.randint(0, width - 1)
         y = random.randint(0, height - 1)
+        computer[y][x] = random.randint(0, INTENSITY)
+
+
+def draw():
+    # Copy the effect to the framebuffer
+    memoryview(graphics)[:] = numpy.ndarray(numpy.clip(computer, 0, 1) * (PALETTE_ENTRIES - 1), dtype=numpy.uint8).tobytes()
+    cu.update(graphics)
+
+
+width = CosmicUnicorn.WIDTH
+height = CosmicUnicorn.HEIGHT
+computer = numpy.zeros((height, width))
+
+t_count = 0
+t_total = 0
+
+
+while True:
+    if cu.is_pressed(CosmicUnicorn.SWITCH_BRIGHTNESS_UP):
+        cu.adjust_brightness(+0.01)
+
+    if cu.is_pressed(CosmicUnicorn.SWITCH_BRIGHTNESS_DOWN):
+        cu.adjust_brightness(-0.01)
+
+    if cu.is_pressed(CosmicUnicorn.SWITCH_VOLUME_DOWN):
+        volume -= 0.1
+        volume = max(0.0, volume)
+        boopety_beepety.volume(volume)
+
+    if cu.is_pressed(CosmicUnicorn.SWITCH_VOLUME_UP):
+        volume += 0.1
+        volume = min(1.0, volume)
+        boopety_beepety.volume(volume)
+
+    tstart = time.ticks_ms()
+    gc.collect()
+    update()
+    draw()
+    tfinish = time.ticks_ms()
+
+    # Play random notes between 100 and 880Hz for a computery effect
+    boopety_beepety.frequency(random.randint(100, 880))
+    boopety_beepety.trigger_attack()
+
+    total = tfinish - tstart
+    t_total += total
+    t_count += 1
+
+    if t_count == 60:
+        per_frame_avg = t_total / t_count
+        print(f"60 frames in {t_total}ms, avg {per_frame_avg:.02f}ms per frame, {1000/per_frame_avg:.02f} FPS")
+        t_count = 0
+        t_total = 0
+
+    # pause for a moment (important or the USB serial device will fail)
+    # try to pace at 60fps or 30fps
+    if total > 1000 / 30:
+        time.sleep(0.0001)
+    elif total > 1000 / 60:
+        t = 1000 / 30 - total
+        time.sleep(t / 1000)
+    else:
+        t = 1000 / 60 - total
+        time.sleep(t / 1000)
