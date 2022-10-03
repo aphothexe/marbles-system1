3efed42b37829ca1b25762a3bbad15d4edfc173f
aphothexe <109735319+aphothexe@users.noreply.github.com>
2022-10-03 19:33:10 +0000

    Update History.md

--- micropython/examples/galactic_unicorn/cheerlights_history.py
+++ micropython/examples/galactic_unicorn/cheerlights_history.py
@@ -65,4 +65,65 @@ def get_data():
 
 
 def update_leds():
-    # light up the 
+    # light up the LEDs
+    # this step takes a second, it's doing a lot of hex_to_rgb calculations!
+    print("Updating LEDs...")
+    i = 0
+    for x in range(width):
+        for y in range(height):
+            r = hex_to_rgb(colour_array[i])[0]
+            g = hex_to_rgb(colour_array[i])[1]
+            b = hex_to_rgb(colour_array[i])[2]
+            current_colour = graphics.create_pen(r, g, b)
+            graphics.set_pen(current_colour)
+            graphics.pixel(x, y)
+            i = i + 1
+    gu.update(graphics)
+    print("LEDs updated!")
+
+
+gu = GalacticUnicorn()
+graphics = PicoGraphics(DISPLAY)
+
+width = GalacticUnicorn.WIDTH
+height = GalacticUnicorn.HEIGHT
+
+gu.set_brightness(0.5)
+
+# set up the Pico W's onboard LED
+pico_led = Pin('LED', Pin.OUT)
+
+current_colour = graphics.create_pen(0, 0, 0)
+
+# set up an list to store the colours
+colour_array = ["#000000"] * 583
+
+# set up wifi
+try:
+    network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, status_handler=status_handler)
+    uasyncio.get_event_loop().run_until_complete(network_manager.client(WIFI_CONFIG.SSID, WIFI_CONFIG.PSK))
+except Exception as e:
+    print(f'Wifi connection failed! {e}')
+
+# get the first lot of data
+get_data()
+
+# start timer (the timer will call the function to update our data every UPDATE_INTERVAL)
+timer = Timer(-1)
+timer.init(period=UPDATE_INTERVAL * 1000, mode=Timer.PERIODIC, callback=lambda t: get_data())
+
+while True:
+    # adjust brightness with LUX + and -
+    # LEDs take a couple of secs to update, so adjust in big (10%) steps
+    if gu.is_pressed(GalacticUnicorn.SWITCH_BRIGHTNESS_UP):
+        gu.adjust_brightness(+0.1)
+        update_leds()
+        print(f"Brightness set to {gu.get_brightness()}")
+
+    if gu.is_pressed(GalacticUnicorn.SWITCH_BRIGHTNESS_DOWN):
+        gu.adjust_brightness(-0.1)
+        update_leds()
+        print(f"Brightness set to {gu.get_brightness()}")
+
+    # pause for a moment (important or the USB serial device will fail)
+    time.sleep(0.001)
