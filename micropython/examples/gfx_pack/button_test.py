672b3686a7fbcb265299d5a5319576fa3c2a24bc
aphothexe <109735319+aphothexe@users.noreply.github.com>
2022-11-29 08:43:13 +0000

    Debug info

--- micropython/examples/gfx_pack/button_test.py
+++ micropython/examples/gfx_pack/button_test.py
@@ -18,4 +18,43 @@ def clear():
 
 
 # set up
-display.set_font("
+display.set_font("bitmap8")
+
+while True:
+    if gp.switch_pressed(SWITCH_A):                       # if a button press is detected...                                          # clear to black
+        gp.set_backlight(255, 0, 0, 0)                    # red, green, blue, white
+        clear()
+        display.text("Button A pressed", 0, 0, WIDTH, 2)  # display some text on the screen
+        display.update()                                  # update the display
+        time.sleep(1)
+    elif gp.switch_pressed(SWITCH_B):
+        gp.set_backlight(255, 125, 0, 0)
+        clear()
+        display.text("Button B pressed", 0, 0, WIDTH, 2)
+        display.update()
+        time.sleep(1)
+    elif gp.switch_pressed(SWITCH_C):
+        gp.set_backlight(0, 255, 0, 0)
+        clear()
+        display.text("Button C pressed", 0, 0, WIDTH, 2)
+        display.update()
+        time.sleep(1)
+    elif gp.switch_pressed(SWITCH_D):
+        gp.set_backlight(0, 0, 255, 0)
+        clear()
+        display.text("Button D pressed", 0, 0, WIDTH, 2)
+        display.update()
+        time.sleep(1)
+    elif gp.switch_pressed(SWITCH_E):
+        gp.set_backlight(255, 0, 255, 0)
+        clear()
+        display.text("Button E pressed", 0, 0, WIDTH, 2)
+        display.update()
+        time.sleep(1)
+    else:
+        gp.set_backlight(0, 0, 0, 125)
+        clear()
+        display.set_pen(15)
+        display.text("Press any button!", 0, 0, WIDTH, 2)
+        display.update()
+    time.sleep(0.01)  # this number is how frequently the Pico checks for button presses
