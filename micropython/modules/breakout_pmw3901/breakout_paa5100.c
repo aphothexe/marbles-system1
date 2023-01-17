d05036987abd28afc3f3a3e977bda26432f9fdf7
aphothexe <109735319+aphothexe@users.noreply.github.com>
2023-01-17 09:16:49 +0000

    fix few bugs

--- micropython/modules/breakout_pmw3901/breakout_paa5100.c
+++ micropython/modules/breakout_pmw3901/breakout_paa5100.c
@@ -27,4 +27,49 @@ STATIC const mp_rom_map_elem_t BreakoutPAA5100_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_DEGREES_180), MP_ROM_INT(0x02) },
     { MP_ROM_QSTR(MP_QSTR_DEGREES_270), MP_ROM_INT(0x03) },
     { MP_ROM_QSTR(MP_QSTR_FRAME_SIZE), MP_ROM_INT(35) },
-    { MP_ROM_QSTR(MP_QSTR_FRAME_BYTES), MP_ROM_INT(1225) 
+    { MP_ROM_QSTR(MP_QSTR_FRAME_BYTES), MP_ROM_INT(1225) },
+};
+STATIC MP_DEFINE_CONST_DICT(BreakoutPAA5100_locals_dict, BreakoutPAA5100_locals_dict_table);
+
+/***** Class Definition *****/
+#ifdef MP_DEFINE_CONST_OBJ_TYPE
+MP_DEFINE_CONST_OBJ_TYPE(
+    breakout_paa5100_BreakoutPAA5100_type,
+    MP_QSTR_BreakoutPAA5100,
+    MP_TYPE_FLAG_NONE,
+    make_new, BreakoutPAA5100_make_new,
+    print, BreakoutPMW3901_print,
+    locals_dict, (mp_obj_dict_t*)&BreakoutPAA5100_locals_dict
+);
+#else
+const mp_obj_type_t breakout_paa5100_BreakoutPAA5100_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_BreakoutPAA5100,
+    .print = BreakoutPMW3901_print,
+    .make_new = BreakoutPAA5100_make_new,
+    .locals_dict = (mp_obj_dict_t*)&BreakoutPAA5100_locals_dict,
+};
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// breakout_paa5100 Module
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/***** Globals Table *****/
+STATIC const mp_map_elem_t breakout_paa5100_globals_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_breakout_paa5100) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_BreakoutPAA5100), (mp_obj_t)&breakout_paa5100_BreakoutPAA5100_type },
+};
+STATIC MP_DEFINE_CONST_DICT(mp_module_breakout_paa5100_globals, breakout_paa5100_globals_table);
+
+/***** Module Definition *****/
+const mp_obj_module_t breakout_paa5100_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&mp_module_breakout_paa5100_globals,
+};
+
+#if MICROPY_VERSION <= 70144
+MP_REGISTER_MODULE(MP_QSTR_breakout_paa5100, breakout_paa5100_user_cmodule, MODULE_BREAKOUT_PMW3901_ENABLED);
+#else
+MP_REGISTER_MODULE(MP_QSTR_breakout_paa5100, breakout_paa5100_user_cmodule);
+#endif
