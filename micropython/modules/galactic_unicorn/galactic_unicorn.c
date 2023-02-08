f25173407aeec4ee7c1bda9960b09c57f145be0b
aphothexe <109735319+aphothexe@users.noreply.github.com>
2023-02-08 16:58:02 +0000

    readme fixes

--- micropython/modules/galactic_unicorn/galactic_unicorn.c
+++ micropython/modules/galactic_unicorn/galactic_unicorn.c
@@ -97,4 +97,54 @@ STATIC MP_DEFINE_CONST_DICT(GalacticUnicorn_locals_dict, GalacticUnicorn_locals_
 #ifdef MP_DEFINE_CONST_OBJ_TYPE
 MP_DEFINE_CONST_OBJ_TYPE(
     Channel_type,
-    MP_QSTR_
+    MP_QSTR_Channel,
+    MP_TYPE_FLAG_NONE,
+    make_new, Channel_make_new,
+    print, Channel_print,
+    locals_dict, (mp_obj_dict_t*)&Channel_locals_dict
+);
+
+MP_DEFINE_CONST_OBJ_TYPE(
+    GalacticUnicorn_type,
+    MP_QSTR_GalacticUnicorn,
+    MP_TYPE_FLAG_NONE,
+    make_new, GalacticUnicorn_make_new,
+    print, GalacticUnicorn_print,
+    locals_dict, (mp_obj_dict_t*)&GalacticUnicorn_locals_dict
+);
+#else
+const mp_obj_type_t Channel_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_Channel,
+    .print = Channel_print,
+    .make_new = Channel_make_new,
+    .locals_dict = (mp_obj_dict_t*)&Channel_locals_dict,
+};
+
+const mp_obj_type_t GalacticUnicorn_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_GalacticUnicorn,
+    .print = GalacticUnicorn_print,
+    .make_new = GalacticUnicorn_make_new,
+    .locals_dict = (mp_obj_dict_t*)&GalacticUnicorn_locals_dict,
+};
+#endif
+
+/***** Globals Table *****/
+STATIC const mp_map_elem_t galactic_globals_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_galactic) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_Channel), (mp_obj_t)&Channel_type },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_GalacticUnicorn), (mp_obj_t)&GalacticUnicorn_type },
+};
+STATIC MP_DEFINE_CONST_DICT(mp_module_galactic_globals, galactic_globals_table);
+
+/***** Module Definition *****/
+const mp_obj_module_t galactic_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&mp_module_galactic_globals,
+};
+#if MICROPY_VERSION <= 70144
+MP_REGISTER_MODULE(MP_QSTR_galactic, galactic_user_cmodule, MODULE_GALACTIC_ENABLED);
+#else
+MP_REGISTER_MODULE(MP_QSTR_galactic, galactic_user_cmodule);
+#endif
